02cdc07bfcdeecddb35874681f804967700b8a8b
Mark <mgius7096@gmail.com>
2011-01-23 15:56:01 -0800

    These files are deprecated

--- items/models.py
+++
@@ -1,8 +0,0 @@
-class Weapon(object):
-   __slots__ = ['name', 'attack', 'element']
-
-class Armor(object):
-   __slots__ = ['name', 'defense', 'element']
-
-class Item(object):
-   __slots__ = ['name', 'effect']
